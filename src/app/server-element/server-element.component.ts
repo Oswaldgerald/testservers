2ea8e9dce7fc215faa62b59e9c99bd5c13446960
GERALD Jr <moswaldgerald@gmail.com>
2020-04-16 23:24:51 +0300

    lifecycle hook

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +6,20 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated,
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input() element: { type: string, name: string, content: string };
+  @Input() name: string;
 
   constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChange is called');
   }
 
   ngOnInit() {
+    console.log('OnInit is called !');
   }
 
 }
